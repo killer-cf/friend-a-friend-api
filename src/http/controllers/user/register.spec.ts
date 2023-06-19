395cc9235e7243603b6f26ca9bad1610ea2adc97
killer-cf <costa.kilder@live.com>
2023-06-19 15:42:10 -0300

    register test

--- src/http/controllers/user/register.spec.ts
+++ src/http/controllers/user/register.spec.ts
@@ -1,5 +1,26 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('teste e2e', () => {
-  expect(1 + 1).toEqual(2)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Jose',
+      email: 'jose@gmail.com',
+      phone: '81999999865',
+      address: '123 Main Street',
+      cep: '50720-570',
+      password: 'password',
+    })
+
+    expect(response.status).toEqual(201)
+  })
 })
