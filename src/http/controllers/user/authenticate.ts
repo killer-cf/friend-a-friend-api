848af737eb189279ecdfd8a175007aa29b015175
killer-cf <costa.kilder@live.com>
2023-06-19 16:19:36 -0300

    user  authenticate controller

---
+++ src/http/controllers/user/authenticate.ts
@@ -0,0 +1,61 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(req: FastifyRequest, res: FastifyReply) {
+  const registerBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    const registerUseCase = makeAuthenticateUseCase()
+
+    const { user } = await registerUseCase.execute({
+      email,
+      password,
+    })
+
+    const token = await res.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    const refreshToken = await res.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return res
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return res.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+}
--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { authenticate } from './authenticate'
 
 export async function userRoutes(app: FastifyInstance) {
-  app.post('/users', { onRequest: [verifyJWT] }, register)
+  app.post('/users', register)
+
+  app.post('/sessions', { onRequest: [verifyJWT] }, authenticate)
 }
