d9fcaa217bd4fde38b1de2248233f1886fe34cc4
killer-cf <costa.kilder@live.com>
2023-06-19 16:05:15 -0300

    config jwt

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
 import { userRoutes } from './http/controllers/user/routes'
+import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.register(userRoutes)
--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function userRoutes(app: FastifyInstance) {
-  app.post('/users', register)
+  app.post('/users', { onRequest: [verifyJWT] }, register)
 }
