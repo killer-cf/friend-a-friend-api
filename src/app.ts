db9ee258e94d2360c411335bb1351abacdedaba4
killer-cf <costa.kilder@live.com>
2023-06-20 16:52:27 -0300

    code status validation error

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
 import fastifyCookie from '@fastify/cookie'
 import { petsRoutes } from './http/controllers/pet/routes'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
@@ -23,3 +24,17 @@ app.register(fastifyCookie)
 app.register(userRoutes)
 
 app.register(petsRoutes)
+
+app.setErrorHandler((error, _, res) => {
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return res.status(500).send({ message: 'Internal server error.' })
+})
